56dff6b2fe540c471c75c5ba23b84e72073f16dd
katy0409 <b900543@gmail.com>
2026-07-15 11:26:30 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'restart-v12.4.1';
+const CACHE = 'restart-v13.1';
 const FILES = [
   './','index.html','manifest.webmanifest','icon-192.png','icon-512.png','coin-icon.png',
-  'css/app.css?v=12.4.1','js/supabase-config.js?v=12.4.1','js/cloud-settings.js?v=12.4.1',
-  'js/auth.js?v=12.4.1','js/app.js?v=12.4.1','js/avatar-v123.js?v=12.4.1','js/square-v13.js?v=12.4.1',
+  'css/app.css?v=13.1','js/supabase-config.js?v=13.1','js/cloud-settings.js?v=13.1',
+  'js/auth.js?v=13.1','js/app.js?v=13.1','js/avatar-v123.js?v=13.1','js/square-v13.js?v=13.1',
   'assets/avatar/body/body_front.png','assets/avatar/body/body_left.png','assets/avatar/body/body_right.png',
   'assets/avatar/body/body_back_left.png','assets/avatar/body/body_back.png','assets/avatar/body/body_back_right.png',
   'assets/avatar/head/head_base.png','assets/avatar/face/male_face_default.png','assets/avatar/face/female_face_default.png',
